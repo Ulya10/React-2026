5b785b4dfd19a52ccfa1007dd156d0e7791c3790
Ulya <ulya.working@yandex.ru>
2026-06-03 19:00:12 +0300

    feat: add basic structure

--- react-forms/src/App.tsx
+++ react-forms/src/App.tsx
@@ -1,14 +1,48 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from './assets/vite.svg';
-import heroImg from './assets/hero.png';
 import './App.css';
 
 function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [formType, setFormType] = useState<'uncontrolled' | 'hookform'>(
+    'uncontrolled'
+  );
+
+  function openModal(type: 'uncontrolled' | 'hookform'): void {
+    setIsModalOpen(true);
+    setFormType(type);
+  }
+
+  function closeModal() {
+    setIsModalOpen(false);
+  }
   return (
-    <>
-      <div>Hello</div>
-    </>
+    <div className="app">
+      <h1>Forms</h1>
+      <div className="buttons">
+        <button className="button" onClick={() => openModal('uncontrolled')}>
+          First form
+        </button>
+        <button className="button" onClick={() => openModal('hookform')}>
+          Second form
+        </button>
+      </div>
+      {isModalOpen && (
+        <div className="overlay">
+          <div className="modal">
+            <h2>
+              {formType === 'uncontrolled'
+                ? 'Uncontrolled Form'
+                : 'React Hook Form'}
+            </h2>
+            <p>Form will be here</p>
+            <button onClick={closeModal}>Close</button>
+          </div>
+        </div>
+      )}
+      <div className="submitted-data">
+        <h2 className="submitted-title">Submitted data</h2>
+      </div>
+    </div>
   );
 }
 
